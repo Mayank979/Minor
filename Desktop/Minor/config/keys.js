df543cc59679528ee18fcd3761c7ba3773dad720
Mayank979 <yadav.mayank800@gmail.com>
2019-05-31 09:31:08 +0530

    Update keys.js

--- Desktop/Minor/config/keys.js
+++ Desktop/Minor/config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
   google: {
     clientID: "166286863027-rtkse87k15s44057c4v78k7ri5tj8cre.apps.googleusercontent.com",
-    clientSecret: "H0_xH5j4nBSK_gXtRaHfbJ9K"
+    clientSecret: "****************************"
   }
 };
